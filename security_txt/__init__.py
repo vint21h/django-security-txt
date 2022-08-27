679b995c5192de36179170af5b0b5980fdeed277
Felix Goyda <felix@menshikov.de>
2022-08-27 14:48:03 +0200

    Remove default_app_config, remove warning in Django4

--- security_txt/__init__.py
+++ security_txt/__init__.py
@@ -2,12 +2,3 @@
 
 # django-security-txt
 # security_txt/__init__.py
-
-
-from typing import List
-
-
-__all__: List[str] = ["default_app_config"]
-
-
-default_app_config: str = "security_txt.apps.DjangoSecurityTxtConfig"
