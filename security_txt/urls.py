977dc5183f1f5666ab5ffea130ab60b545f02862
Felix Goyda <felix@menshikov.de>
2022-08-27 14:48:25 +0200

    use path in urls.py, add content_type as per rfc

--- security_txt/urls.py
+++ security_txt/urls.py
@@ -6,7 +6,7 @@
 
 from typing import List, Union
 
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 from django.urls.resolvers import URLPattern, URLResolver
 
@@ -18,11 +18,15 @@ __all__ = ["urlpatterns"]
 
 # security.txt urls
 urlpatterns: List[Union[URLPattern, URLResolver]] = [
-    url(
-        r"^$",
+    path(
+        "",
         TemplateView.as_view(
             template_name="security_txt/security_txt.txt",
             extra_context={"SIGN": settings.SECURITY_TXT_SIGN},  # type: ignore
+            # See: https://datatracker.ietf.org/doc/id/draft-foudil-securitytxt-10.txt
+            # It MUST have a Content-Type of "text/plain" with the default charset
+            # parameter set to "utf-8" (as per section 4.1.3 of [RFC2046]).
+            content_type="text/plain",
         ),
         name="security-txt",
     ),
