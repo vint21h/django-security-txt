294bfa440173fa719476c1bcaee1b116ed2ed473
Felix Goyda <felix@menshikov.de>
2022-08-27 22:29:21 +0200

    fix template issue, add extend urls test

--- tests/urls/test_urls.py
+++ tests/urls/test_urls.py
@@ -9,13 +9,27 @@ from typing import List
 from django.test import TestCase
 from http import HTTPStatus
 
+from security_txt.models.acknowledgment import Acknowledgment
+from security_txt.models.contact import Contact
+
 __all__: List[str] = ["URLTest"]
 
 class URLTest(TestCase):
     """ test urls.py """
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        """Set up non-modified objects used by all test methods."""
+        Acknowledgment.objects.create(url="https://example.com/")
+        Contact.objects.create(
+            type=Contact.TYPE_EMAIL, email="test@example.com"
+        )
 
     def test_url(self) -> None:
         """ Test status_code and content_type """
         response = self.client.get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response["content-type"], "text/plain")
+        self.assertContains(response, "test@example.com")
+        self.assertContains(response, "https://example.com")
+
